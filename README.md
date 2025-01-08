cffca4a76cc354e6eed1ddb7e123a81aa912feec
slvonderheide <slvonderheide@gmail.com>
2025-01-08 11:07:36 -0500

    updated files and updated README

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
-        # Tilly Time
+        # "TaskTracker Pro" (a task management tool)
 
 ## Description
-When my dog plays.
+"TaskTracker Pro is a lightweight task management app designed to help individuals and teams stay organized. It features task prioritization, deadline reminders, and a collaborative workspace."
 
 ## Table of Contents
 - [Installation](#installation)
@@ -13,22 +13,27 @@ When my dog plays.
 - [Questions](#questions)
 
 ## Installation
-download product
+ 1. Clone the repository 2. Navigate to the project folder 3. Install dependencies 4. Run the project
 
 ## Usage
-use when needed
+"After starting the application, log in to your account. Click 'Add Task' to create a new task. Use the dashboard to track progress and set deadlines."
 
 ## Contributing
-no idea
+We welcome contributions! Please follow these steps: 1. Fork the repository. 2. Create a new branch: `git checkout -b feature-name`. 3. Make your changes and commit them. 4. Push the changes: `git push origin feature-name`. 5. Submit a pull request. 
 
 ## Tests
-run it to see
+"Run the following command to execute tests: npm test. Make sure all tests pass before submitting any changes."
 
 ## License
-MIT
+"This project is licensed under the MIT License. See the LICENSE file for details."
 
 ## Questions
 If you have any questions, feel free to reach out to me:
-- GitHub: [slvonderheide](https://github.com/slvonderheide)
-- Email: slvondere
+- GitHub: [Slvonderheide](https://github.com/Slvonderheide)
+- Email: slvonderheide@gmail.com
+
+## Video
+Link to video of how to use: https://drive.google.com/file/d/1uJx5wLWyZ4BllPyxUeFvRFkoKHGero7n/view?usp=sharing
+
+
         
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (license === "Apache 2.0") {
+    return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  } else if (license === "GPL 3.0") {
+    return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  } else if (license === "BSD 3") {
+    return `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `[MIT](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Apache 2.0") {
+    return `[Apache 2.0](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "GPL 3.0") {
+    return `[GPL 3.0](https://opensource.org/licenses/GPL-3.0)`;
+  } else if (license === "BSD 3") {
+    return `[BSD 3](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `## License
+    This project is licensed under the ${license} license.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
